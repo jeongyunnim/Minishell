abfcfcd1c7294b3102feea5732198e03b90bc1ca
jeongyunnim <jeongyun7521@gmail.com>
2023-03-14 02:00:38 +0900

    builtin 실행 테스트 가능(pipe는 준비 중)

--- exec_commands.c
+++ exec_commands.c
@@ -103,46 +103,21 @@ int	heredoc_handler(t_info *info)
 int	isbuiltin(char *cmd)
 {
 	if (ft_strncmp("echo", cmd, 5) == 0)
-	{
-		write(1, "이 자리에 echo 와야 함\n", 30);
-		//echo 실행
-	}
+		return (1);
 	else if (ft_strncmp("cd", cmd, 3) == 0)
-	{
-		write(1, "이 자리에 ㅊㅇ 와야 함\n", 28);
-		//cd 실행
-	}
+		return (1);
 	else if (ft_strncmp("pwd", cmd, 4) == 0)
-	{
-		write(1, "이 자리에 pwd 와야 함\n", 29);
-		//pwd 실행
-	}
+		return (1);
 	else if (ft_strncmp("export", cmd, 7) == 0)
-	{
-		write(1, "이 자리에 export 와야 함\n", 32);
-		//export 실행
-	}
+		return (1);
 	else if (ft_strncmp("unset", cmd, 6) == 0)
-	{
-		write(1, "이 자리에 unset 와야 함\n", 31);
-		//unset 실행
-	}
+		return (1);
 	else if (ft_strncmp("env", cmd, 4) == 0)
-	{
-		write(1, "이 자리에 env 와야 함\n", 29);
-		//env 실행
-	}
+		return (1);
 	else if (ft_strncmp("exit", cmd, 5) == 0)
-	{
-		write(1, "이 자리에 exit 와야 함\n", 30);
-		//exit 실행
-	}
+		return (1);
 	else
-	{
-		write(1, "빌트인 처리로 감\n", 24);
 		return (0);
-	}
-	return (1);
 }
 
 void	parent_process_wait(void)
@@ -152,6 +127,46 @@ void	parent_process_wait(void)
 	status = 0;
 	waitpid(-1, &status, 0);
 	printf("status %d\n", status);
+}
+
+int	exec_builtin(t_cmd_deque *cmd_line)
+{
+	if (ft_strncmp("echo", cmd_line->head->commands_args[0], 5) == 0)
+	{
+		write(1, "이 자리에 echo 와야 함\n", 30);
+		//echo 실행
+	}
+	else if (ft_strncmp("cd", cmd_line->head->commands_args[0], 3) == 0)
+	{
+		write(1, "이 자리에 cd 와야 함\n", 28);
+		//cd 실행
+	}
+	else if (ft_strncmp("pwd", cmd_line->head->commands_args[0], 4) == 0)
+	{
+		write(1, "이 자리에 pwd 와야 함\n", 29);
+		//pwd 실행
+	}
+	else if (ft_strncmp("export", cmd_line->head->commands_args[0], 7) == 0)
+	{
+		write(1, "이 자리에 export 와야 함\n", 32);
+		//export 실행
+	}
+	else if (ft_strncmp("unset", cmd_line->head->commands_args[0], 6) == 0)
+	{
+		write(1, "이 자리에 unset 와야 함\n", 31);
+		//unset 실행
+	}
+	else if (ft_strncmp("env", cmd_line->head->commands_args[0], 4) == 0)
+	{
+		write(1, "이 자리에 env 와야 함\n", 29);
+		//env 실행
+	}
+	else if (ft_strncmp("exit", cmd_line->head->commands_args[0], 5) == 0)
+	{
+		write(1, "이 자리에 exit 와야 함\n", 30);
+		//exit 실행
+	}
+	return (0);
 }
 
 int	exec_commands(t_info *info)
@@ -177,6 +192,7 @@ int	exec_commands(t_info *info)
 		}
 		if (info->pipes == 0 && isbuiltin(info->cmds->head->commands_args[0]) == 1)
 		{
+			exec_builtin(info->cmds->head);
 			//포크뜨지 않기.. 그래야 export a=1 같은 것이 저장이 된다..... ㅜㅜ 
 			//바로 실행
 			return (0);
